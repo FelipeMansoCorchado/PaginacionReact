5b8a9b572afc1c311c352398d558f4d727694a75
FelipeMansoCorchado <48017865+FelipeMansoCorchado@users.noreply.github.com>
2020-03-27 18:44:53 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Empleados from "./components/Empleados";
+
+function App() {
+  return (
+    <>
+      <Empleados></Empleados>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Global.js
@@ -0,0 +1,4 @@
+var Global = {
+  urlpost: "https://apiempleadosfullstack.azurewebsites.net"
+};
+export default Global;
